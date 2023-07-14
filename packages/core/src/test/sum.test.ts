3fae0cc9c402cc3f6f7ea25c7d533c1bbe05a7a3
Lucas Rego <lucas.sr.rg@gmail.com>
2023-07-14 20:13:12 -0300

    Removed core package, It will be a shared package

--- packages/core/src/test/sum.test.ts
+++
@@ -1,9 +0,0 @@
-import sum from './index.js'
-import assert from 'node:assert'
-import {describe, it} from 'node:test'
-
-describe('sum', () => {
-    it('adds two numbers', () => {
-        assert.strictEqual(sum(1, 2), 3);
-    });
-})
