da805b5f0462b067cd14b98bbf4c375e5e750585
Lucas Rego <lucas.sr.rg@gmail.com>
2023-07-14 20:12:31 -0300

    Altered. For now, It will be a todo task

--- packages/video-catalog/src/index.ts
+++ packages/video-catalog/src/index.ts
@@ -1,11 +1,1 @@
-import sum from '@devflix/core/test';
-
-const convertSumToString = (a: number):string => {
-    const b = sum(a, 2);
-    return b.toString();
-}
-
-console.log(convertSumToString(2)   );
-
-
-export default convertSumToString;
+console.log('toDo');
